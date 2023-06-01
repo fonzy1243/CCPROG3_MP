f29854ab66697cadffce3047063c3d8a55371488
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-01 10:07:39 +0800

    Added calories attribute, getName, getPrice, and constructor

--- src/Item.java
+++ src/Item.java
@@ -2,4 +2,20 @@ public class Item
 {
 	private String name;
 	private int price;
+	private final int calories;
+
+	public Item(String name, int priceInCents, int calories)
+	{
+		this.name = name;
+		this.price = priceInCents;
+		this.calories = calories;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getPrice()
+	{
+		return price;
+	}
 }
