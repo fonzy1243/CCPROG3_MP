8d45cff080a58fb20f5cf0013937afe4574a7139
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-01 10:08:39 +0800

    Added initItemCount, constructor, list getter and addItem

--- src/Slot.java
+++ src/Slot.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Slot
 {
+	private static final int initItemCount = 10;
 	private List<Item> itemList;
+
+	public Slot()
+	{
+		itemList = new ArrayList<>(initItemCount);
+	}
+
+	public List<Item> getItemList()
+	{
+		return this.itemList;
+	}
+
+	public void addItem(Item newItem, int quantity)
+	{
+		for (int i = 0; i < quantity; i++)
+		{
+			itemList.add(newItem);
+		}
+	}
 }
