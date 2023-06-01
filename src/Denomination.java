784cfa91ace3393ceb0375989ec2be028187c385
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-02 06:54:57 +0800

    Added class for currency denominations

---
+++ src/Denomination.java
@@ -0,0 +1,11 @@
+public class Denomination
+{
+	private final int value;
+	private int quantity;
+
+	public Denomination(int value, int quantity)
+	{
+		this.value = value;
+		this.quantity = quantity;
+	}
+}
