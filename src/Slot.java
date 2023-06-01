c7517129977856f5607681bfcc9df156f0502a14
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-01 22:27:27 +0800

    Moved the test method to slot (shouldn't matter much, just for testing)

--- src/Slot.java
+++ src/Slot.java
@@ -23,5 +23,18 @@ public class Slot
 		}
 	}
 
-
+	// for testing
+	// actually we should probably move this back to slot
+	public void printSlotItems()
+	{
+		int count = 0;
+		for (Item item : itemList)
+		{
+			count++;
+			System.out.println("Item " + count + ":");
+			System.out.println("Item name: "+ item.getName());
+			System.out.println("Item price: " + (float) item.getPrice() / 100);
+			System.out.println("Item calories: " + item.getCalories());
+		}
+	}
 }
