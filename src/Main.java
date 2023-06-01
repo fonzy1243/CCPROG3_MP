7f727665d606071809014cf8b16dcdbb67ac4301
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-01 16:22:13 +0800

    Added test run

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!");
+		RegularVendingMachine machine1 = new RegularVendingMachine(8);
+
+		Item newItem = new Item("deez nuts", 69699, 69);
+		Item reverse = new Item("nuts deez", 9696, 96);
+		machine1.addItemToSlot(newItem, 0, 3);
+		machine1.addItemToSlot(reverse, 0, 2);
+
+		machine1.printSlotItems(0);
 	}
 }
