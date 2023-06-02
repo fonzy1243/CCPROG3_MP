8d06539af10a9dcb80d38b9cd464f561abc6f997
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-02 08:51:50 +0800

    Changed Denomination to include a hashmap

--- src/Denomination.java
+++ src/Denomination.java
@@ -1,11 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Denomination
 {
-	private final int value;
-	private int quantity;
+	private Map<Integer, Integer> denominationStock;
 
-	public Denomination(int value, int quantity)
+	public Denomination()
 	{
-		this.value = value;
-		this.quantity = quantity;
+		denominationStock = new HashMap<>();
+
+		// Centavo coins
+		denominationStock.put(1, 1000);
+		denominationStock.put(5, 1000);
+		denominationStock.put(25, 1000);
+
+		// Peso bills
+		denominationStock.put(100, 1000);
+		denominationStock.put(500, 1000);
+		denominationStock.put(1000, 1000);
+
+		// Peso bills
+		denominationStock.put(2000, 500);
+		denominationStock.put(5000, 500);
+		denominationStock.put(10000, 500);
+		denominationStock.put(50000, 500);
+		denominationStock.put(100000, 500);
+	}
+
+	public Map<Integer, Integer> getDenominationStock()
+	{
+		return denominationStock;
+	}
+
+	public void addDenomination(int value, int quantity)
+	{
+		if (!denominationStock.containsKey(value))
+		{
+
+			return;
+		}
 	}
 }
