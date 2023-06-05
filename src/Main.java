575f32c1bbbd0701d40db58ad99808acbbb1b1b5
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-05 22:57:31 +0800

    Initial JavaFX window

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
+import javafx.application.Application;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		RegularVendingMachine machine1 = new RegularVendingMachine(8);
+		VendingMachine model = new RegularVendingMachine(8);
+		VendingMachineController controller = new VendingMachineController(model);
 
-		Item newItem = new Item("deez nuts", 69699, 69);
-		Item reverse = new Item("nuts deez", 9696, 96);
-		machine1.addItemToSlot(newItem, 0, 3);
-		machine1.addItemToSlot(reverse, 0, 2);
-
-		machine1.getSlots()[0].printSlotItems();
-		System.out.println("Deez nuts count: " + machine1.getAvailability("deez nuts"));
-
-		machine1.getSlots()[0].printSlotItems();
-		System.out.println("Deez nuts count: " + machine1.getAvailability("deez nuts"));
-
-		machine1.dispenseItem(0, 71325);
+		Application.launch(VendingMachineViewer.class, args);
 	}
 }
--- src/VendingMachineViewer.java
+++ src/VendingMachineViewer.java
@@ -1,4 +1,24 @@
-public class VendingMachineViewer
-{
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class VendingMachineViewer extends Application
+{
+	private VendingMachineController controller;
+
+	public VendingMachineViewer()
+	{}
+
+	public VendingMachineViewer(VendingMachineController controller)
+	{
+		this.controller = controller;
+	}
+
+
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+
+
+		stage.show();
+	}
 }
