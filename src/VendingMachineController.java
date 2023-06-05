306ace56ce02eb01af0f96ac246df2d741f90fe1
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-05 13:57:44 +0800

    Created new VendingMachineController class.

---
+++ src/VendingMachineController.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class VendingMachineController
+{
+	private VendingMachine model;
+
+	public VendingMachineController(VendingMachine model)
+	{
+		this.model = model;
+	}
+
+	public void purchaseItem()
+	{
+		// model.dispenseItem();
+	}
+}
