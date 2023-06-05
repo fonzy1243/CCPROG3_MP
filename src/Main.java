f8e6dd34f6ce2f8732f833ee6fdf888d01fef1b9
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-06 02:00:28 +0800

    Changed main method to contain Application.launch

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		VendingMachine model = new RegularVendingMachine(8);
-		VendingMachineController controller = new VendingMachineController(model);
-
 		Application.launch(VendingMachineViewer.class, args);
 	}
 }
