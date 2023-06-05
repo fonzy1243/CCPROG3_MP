96a9df6482b9938fa48f7647034c4cd038eec86b
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-06 02:01:17 +0800

    Updated VendingMachineViewer to initial scene and font initialization.

--- src/VendingMachineViewer.java
+++ src/VendingMachineViewer.java
@@ -1,24 +1,39 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class VendingMachineViewer extends Application
 {
 	private VendingMachineController controller;
 
 	public VendingMachineViewer()
-	{}
-
-	public VendingMachineViewer(VendingMachineController controller)
 	{
-		this.controller = controller;
+		this.controller = new VendingMachineController();
 	}
-
 
 	@Override
 	public void start(Stage stage) throws Exception
 	{
+		try
+		{
+			Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/MainMenu.fxml")));
 
+			Scene scene = new Scene(root, 720, 720);
 
-		stage.show();
+			Font defaultFont = Font.loadFont(getClass().getResourceAsStream("fonts/Nunito-Regular.ttf"), 12);
+
+			stage.setScene(scene);
+			stage.setTitle("Vending Machine Factory Simulator");
+			stage.show();
+		}
+		catch (Exception exception)
+		{
+			exception.printStackTrace();
+		}
 	}
 }
