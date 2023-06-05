30abda4dd3e31e6a8866846d851c64e5c8df4611
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-05 17:49:46 +0800

    Changed test code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 	{
 		RegularVendingMachine machine1 = new RegularVendingMachine(8);
 
-		Item newItem = new Item("deez nuts", 69699, 69);
+		Item newItem = new Item("deez nuts", 50000, 69);
 		Item reverse = new Item("nuts deez", 9696, 96);
 		machine1.addItemToSlot(newItem, 0, 3);
 		machine1.addItemToSlot(reverse, 0, 2);
@@ -15,6 +15,6 @@ public class Main
 		machine1.getSlots()[0].printSlotItems();
 		System.out.println("Deez nuts count: " + machine1.getAvailability("deez nuts"));
 
-		machine1.dispenseItem(0, 71325);
+		machine1.dispenseItem(0, 92500);
 	}
 }
