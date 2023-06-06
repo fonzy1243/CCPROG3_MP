37d53c2fb57d9e7fe1573efa3b39b7cfdeb0a309
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-07 03:38:58 +0800

    Added class for button animation.

---
+++ src/Controller/ButtonAnimator.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.animation.ScaleTransition;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+public class ButtonAnimator
+{
+	public void resizeWhenHovered(Button button)
+	{
+		ScaleTransition newScale = new ScaleTransition(Duration.millis(25));
+		newScale.setToX(1.01);
+		newScale.setToY(1.01);
+		newScale.setNode(button);
+
+		ScaleTransition oldScale = new ScaleTransition(Duration.millis(25));
+		oldScale.setToX(1);
+		oldScale.setToY(1);
+		oldScale.setNode(button);
+
+		button.setOnMouseEntered(mouseEvent -> newScale.playFromStart());
+
+		button.setOnMouseExited(mouseEvent -> oldScale.playFromStart());
+	}
+}
