7f1256ee61800f84c1b83d86f1a81902f212b0e1
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-14 14:17:11 +0800

    Optimized imports.

--- src/Controller/ButtonAnimator.java
+++ src/Controller/ButtonAnimator.java
@@ -2,6 +2,7 @@ package Controller;
 
 import javafx.animation.ScaleTransition;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class ButtonAnimator
