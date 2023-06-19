f658fdf8387d63fbbf135d2f348549fff50c2424
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-19 23:30:31 +0800

    Added new FXML fields.

--- src/Controller/WithdrawInterfaceController.java
+++ src/Controller/WithdrawInterfaceController.java
@@ -5,23 +5,31 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.List;
 
 public class WithdrawInterfaceController extends MenuController
 {
-	public VBox withdrawInterfaceVbox;
+	@FXML
+	private VBox withdrawInterfaceVbox;
+	@FXML
+	private Label moneyLabel;
+	@FXML
+	private TextField moneyTextField;
+	@FXML
+	private Button addButton;
 	@FXML
 	private HBox navButtons;
 	@FXML
 	private AnchorPane topBar;
 	@FXML
 	private Button minimizeButton;
-	@FXML
-	private Button backButton;
 
 	private VendingMachineController vendingMachineController;
 	private final ButtonAnimator buttonAnimator;
@@ -38,7 +46,7 @@ public class WithdrawInterfaceController extends MenuController
 
 	public void openWithdrawInterface()
 	{
-		withdrawInterfaceVbox.setSpacing(15);
+		withdrawInterfaceVbox.setSpacing(35);
 
 		moveApp(topBar, stage);
 		minimizeButton.setOnAction(actionEvent -> minimizeApp(stage));
@@ -49,6 +57,38 @@ public class WithdrawInterfaceController extends MenuController
 		}
 
 		navButtons.setSpacing(7);
+
+		final double[] totalMoney = {vendingMachineController.getVendingMachines().getLast().getDenominations()
+				                             .calculateTotal() / 100.0};
+
+		moneyLabel.setText("₱" + totalMoney[0]);
+
+		addButton.setOnAction(event ->
+		{
+			if ((int) (Float.parseFloat(moneyTextField.getText()) * 100) <= 0)
+			{
+				openPopup("Invalid withdrawal.");
+				return;
+			}
+
+			List<Integer> change = vendingMachineController.getVendingMachines().getLast()
+					.withdrawMoney((int) (Float.parseFloat(moneyTextField.getText()) * 100));
+
+			float total = 0;
+
+			for (Integer currency : change)
+			{
+				total += (float) currency / 100;
+			}
+
+			StringBuilder stringBuilder = new StringBuilder();
+			change.forEach((changeValue) -> stringBuilder.append("₱").append((float) changeValue / 100).append(" "));
+
+			totalMoney[0] -= total;
+			moneyLabel.setText("₱" + totalMoney[0]);
+
+			openPopup("You have withdrawn ₱" + total + ": " + stringBuilder);
+		});
 	}
 
 	@FXML
