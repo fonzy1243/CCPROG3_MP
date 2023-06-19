314e295b810671572bfbf06d1da210e961d53dc2
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-06-19 18:58:03 +0800

    Finished price changing functionality.

--- src/Controller/PriceInterfaceController.java
+++ src/Controller/PriceInterfaceController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Item;
 import Model.Slot;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,24 +8,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PriceInterfaceController extends MenuController
 {
 	public HBox navButtons;
+	public Label priceAmountLabel;
+	public TextField priceTextField;
+	public Button addButton;
 	@FXML
 	private AnchorPane topBar;
 	@FXML
 	private Button minimizeButton;
 	@FXML
 	private VBox priceInterfaceVbox;
-	@FXML
-	private Button backButton;
 	@FXML
 	private ComboBox<Slot> slotDropdown;
 
@@ -71,6 +76,58 @@ public class PriceInterfaceController extends MenuController
 		}
 
 		navButtons.setSpacing(7);
+
+		slotDropdown.setOnAction(event ->
+		{
+			if (slotDropdown.getValue().getItemList().size() > 0)
+			{
+				priceAmountLabel.setText("₱" + (float) (slotDropdown.getValue().getItemList().get(0).getPrice() / 100));
+			}
+			else
+			{
+				priceAmountLabel.setText("₱0");
+			}
+		});
+
+		addButton.setOnAction(event ->
+		{
+			if (slotDropdown.getValue().getItemList() == null || slotDropdown.getValue().getItemList().size() == 0)
+			{
+				openPopup("No items in slot.");
+				return;
+			}
+			if ((int) (Float.parseFloat(priceTextField.getText()) * 100) <= 0)
+			{
+				openPopup("Invalid price.");
+				return;
+			}
+			if (slotDropdown.getValue().getItemList().size() > 0)
+			{
+				changeItemPrices();
+			}
+		});
+	}
+
+	private void changeItemPrices()
+	{
+		int i = 0, slotIndex = -1;
+
+		for (Slot slot : vendingMachineController.getVendingMachines().getLast().getSlots())
+		{
+			if (slot == slotDropdown.getValue())
+			{
+				slotIndex = i;
+			}
+
+			i++;
+		}
+
+		for (Item item : vendingMachineController.getVendingMachines().getLast()
+				.getSlots()[slotIndex].getItemList())
+		{
+			item.setPrice((int) (Float.parseFloat(priceTextField.getText()) * 100));
+			priceAmountLabel.setText("₱" + (float) (slotDropdown.getValue().getItemList().get(0).getPrice() / 100));
+		}
 	}
 
 	@FXML
